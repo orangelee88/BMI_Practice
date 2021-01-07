4f36a34aa033f1d8a413dd3b96ee0be3f7c78297
OrangeLee <a0487387521@gmail.com>
2021-01-07 20:39:04 +0800

    use method practice Bmi

---
+++ app/src/main/java/com/example/bmi_practice/Bmi_method.java
@@ -0,0 +1,60 @@
+package com.example.bmi_practice;
+
+import android.content.SharedPreferences;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Bmi_method {
+
+    public String compute(String gw, String gh) {
+        double return_bmi;
+        double value_w = Double.parseDouble( gw );/**型別轉換String>>Double*/
+        double value_h = Double.parseDouble( gh );/**型別轉換String>>Double*/
+
+        value_h = value_h / 100;/**身高單位換算公尺*/
+        value_h = value_h * value_h;  /**身高的平方*/
+        return_bmi = value_w / value_h;/**BMI公式*/
+        String string_bmi = Double.toString( return_bmi );
+
+        return string_bmi; /**回傳BMI*/
+    }
+
+    public int zero(String gw, String gh) {/** 判斷空值*/
+
+        if (gh.equals( "" ) || gw.equals( "" )) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public String result(String gw, String gh) {/**BMI診斷結果*/
+
+        double return_bmi;
+        double value_w = Double.parseDouble( gw );/**型別轉換String>>Double*/
+        double value_h = Double.parseDouble( gh );/**型別轉換String>>Double*/
+        value_h = value_h / 100;/**身高換算公尺*/
+        value_h = value_h * value_h;  /**身高的平方*/
+        return_bmi = value_w / value_h;/**BMI公式*/
+        String string_bmi = Double.toString( return_bmi );
+        double result_a = Double.parseDouble( string_bmi );
+         String x= "default";
+        if (result_a < 18.5) {
+           x=( "體重過輕" );
+        } else if (18.5 <= result_a && result_a < 24) {
+            x=( "正常範圍" );
+        } else if (24 <= result_a && result_a < 27) {
+           x=( "過    重" );
+        } else if (27 <= result_a && result_a < 30) {
+           x=( "輕度肥胖" );
+        } else if (30 <= result_a && result_a < 35) {
+           x=( "中度肥胖" );
+        } else if (result_a >= 35) {
+           x=( "重度肥胖" );
+        }
+        return x;
+    }
+
+}
---
+++ app/src/main/java/com/example/bmi_practice/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.bmi_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText he;
+    EditText we;
+    public static Context context;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        he = findViewById( R.id.edit1 );/** 把使用者身高資料放到he*/
+        we = findViewById( R.id.edit2 );/** 把使用者體重資料放到we*/
+        Button button = findViewById( R.id.btn1 );
+        button.setOnClickListener( this );
+
+    }
+
+    public void onClick(View V) {  /**按完按鈕後觸發點擊事件*/
+
+        String height = he.getText().toString();/**型別轉換(he)EditText>>String*/
+        String weight = we.getText().toString();/**型別轉換(we)EditText>>String*/
+        Bmi_method bmi_method = new Bmi_method();/**新增一個物件*/
+        int judge = bmi_method.zero( weight, height );/**呼叫bmi_method.zero方法*/
+        if (judge == 1) {
+            Toast.makeText( MainActivity.this, "欄位空白，請重新輸入", Toast.LENGTH_SHORT ).show();
+        } else if (judge == 2) {
+            String a = bmi_method.compute( weight, height );/**呼叫bmi_method.compute方法*/
+            TextView text_bmi = findViewById( R.id.text_bmi );
+            text_bmi.setText(a);
+            String b = bmi_method.result ( weight, height );/**呼叫**BMI診斷結果方法*/
+            TextView text_result = findViewById( R.id.text_result );
+            text_result.setText(b);
+
+        }
+    }
+}
